85c88cf204a6d3d77724713bdc9f368dac87c96e
Tezen96 <96522837+Tezen96@users.noreply.github.com>
2024-07-09 14:46:45 +0545

    Update insert.php

--- insert.php
+++ insert.php
@@ -34,7 +34,7 @@
         $username = "root";
         $password = "";
         $dbname = "my_CRUD";
-        $port = 3300;
+        $port = 3306;
         $conn = mysqli_connect($servername, $username, $password, $dbname, $port);
 
         // Check connection
